e5f8d0aa7d17b689edf36ea716efec0d28c50f20
marcosg432 <mg9149303@gmail.com>
2026-02-09 09:08:01 -0400

    Alterar porta do servidor para 3011 e adicionar og:site_name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3011;
 
 // Servir arquivos estáticos
 app.use(express.static(path.join(__dirname)));
